13be6308103181848b5e719a7822a59d01a06da6
Nitro <nitro0@naver.com>
2020-09-22 22:55:38 +0900

    V1 upload

---
+++ CoupangImage/CoupangImage/Form1.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace CoupangImage {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.rootPath = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.f1RTB = new System.Windows.Forms.RichTextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.f1Box = new System.Windows.Forms.TextBox();
+            this.F2Box = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.f2RTB = new System.Windows.Forms.RichTextBox();
+            this.f1Label = new System.Windows.Forms.Label();
+            this.f2Label = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.othersRTB = new System.Windows.Forms.RichTextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rootPath
+            // 
+            this.rootPath.Dock = System.Windows.Forms.DockStyle.Left;
+            this.rootPath.Location = new System.Drawing.Point(0, 0);
+            this.rootPath.Name = "rootPath";
+            this.rootPath.Size = new System.Drawing.Size(638, 20);
+            this.rootPath.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.startBtn);
+            this.panel1.Controls.Add(this.rootPath);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(770, 22);
+            this.panel1.TabIndex = 1;
+            // 
+            // startBtn
+            // 
+            this.startBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.startBtn.Location = new System.Drawing.Point(638, 0);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(132, 20);
+            this.startBtn.TabIndex = 1;
+            this.startBtn.Text = "시작";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(756, 241);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // f1RTB
+            // 
+            this.f1RTB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.f1RTB.Location = new System.Drawing.Point(3, 28);
+            this.f1RTB.Name = "f1RTB";
+            this.f1RTB.Size = new System.Drawing.Size(764, 56);
+            this.f1RTB.TabIndex = 4;
+            this.f1RTB.Text = "";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.90909F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 79.09091F));
+            this.tableLayoutPanel1.Controls.Add(this.f1Box, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.F2Box, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 295);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(770, 51);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // f1Box
+            // 
+            this.f1Box.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.f1Box.Location = new System.Drawing.Point(163, 3);
+            this.f1Box.Name = "f1Box";
+            this.f1Box.Size = new System.Drawing.Size(604, 20);
+            this.f1Box.TabIndex = 0;
+            this.f1Box.Text = "대표";
+            this.f1Box.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // F2Box
+            // 
+            this.F2Box.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.F2Box.Location = new System.Drawing.Point(163, 28);
+            this.F2Box.Name = "F2Box";
+            this.F2Box.Size = new System.Drawing.Size(604, 20);
+            this.F2Box.TabIndex = 1;
+            this.F2Box.Text = "상세";
+            this.F2Box.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "필터 1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "필터 2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.f2RTB, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.f1RTB, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.f1Label, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.f2Label, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.label5, 0, 4);
+            this.tableLayoutPanel2.Controls.Add(this.othersRTB, 0, 5);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 346);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 6;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(770, 264);
+            this.tableLayoutPanel2.TabIndex = 5;
+            // 
+            // f2RTB
+            // 
+            this.f2RTB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.f2RTB.Location = new System.Drawing.Point(3, 115);
+            this.f2RTB.Name = "f2RTB";
+            this.f2RTB.Size = new System.Drawing.Size(764, 57);
+            this.f2RTB.TabIndex = 5;
+            this.f2RTB.Text = "";
+            // 
+            // f1Label
+            // 
+            this.f1Label.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.f1Label.Location = new System.Drawing.Point(3, 0);
+            this.f1Label.Name = "f1Label";
+            this.f1Label.Size = new System.Drawing.Size(764, 25);
+            this.f1Label.TabIndex = 6;
+            this.f1Label.Text = "label3";
+            this.f1Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // f2Label
+            // 
+            this.f2Label.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.f2Label.Location = new System.Drawing.Point(3, 87);
+            this.f2Label.Name = "f2Label";
+            this.f2Label.Size = new System.Drawing.Size(764, 25);
+            this.f2Label.TabIndex = 7;
+            this.f2Label.Text = "label4";
+            this.f2Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Location = new System.Drawing.Point(3, 175);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(764, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "나머지";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // othersRTB
+            // 
+            this.othersRTB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.othersRTB.Location = new System.Drawing.Point(3, 203);
+            this.othersRTB.Name = "othersRTB";
+            this.othersRTB.Size = new System.Drawing.Size(764, 58);
+            this.othersRTB.TabIndex = 9;
+            this.othersRTB.Text = "";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tabControl1.Location = new System.Drawing.Point(0, 22);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(770, 273);
+            this.tabControl1.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(762, 247);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "필터1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(762, 247);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "필터2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView2.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(756, 241);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.dataGridView3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(762, 247);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "나머지";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView3.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(756, 241);
+            this.dataGridView3.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(770, 626);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Cupang Image";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox rootPath;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RichTextBox f1RTB;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox f1Box;
+        private System.Windows.Forms.TextBox F2Box;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.RichTextBox f2RTB;
+        private System.Windows.Forms.Label f1Label;
+        private System.Windows.Forms.Label f2Label;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox othersRTB;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.DataGridView dataGridView3;
+    }
+}
+
---
+++ CoupangImage/CoupangImage/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+using System.IO;
+using System.Linq;
+using System.Drawing;
+
+namespace CoupangImage {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+            MinimumSize = new Size(400, 600);
+            Form1_Resize(this, null);
+            textBox_TextChanged(this, null);
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            var allowedExtensions = new[] { ".png", ".jpg", ".gif" };
+            var allFiles = Directory
+                .GetFiles(rootPath.Text, "*.*", SearchOption.AllDirectories)
+                .Where(file => allowedExtensions.Any(file.ToLower().EndsWith))
+                .ToList();
+
+            // Grab and listing
+            List<Item> itemsF1 = new List<Item>();
+            List<Item> itemsF2 = new List<Item>();
+            List<Item> itemsOther = new List<Item>();
+            foreach (string file in allFiles) {
+                if (Path.GetFileName(file).Contains(f1Box.Text)) {
+                    itemsF1.Add(new Item {
+                        fileName = Path.GetFileName(file),
+                        path = file.Replace(Path.GetFullPath(Path.Combine(rootPath.Text, @"..\")), "")
+                    }); 
+                } else if (Path.GetFileName(file).Contains(F2Box.Text)) {
+                    itemsF2.Add(new Item {
+                        fileName = Path.GetFileName(file),
+                        path = file.Replace(Path.GetFullPath(Path.Combine(rootPath.Text, @"..\")), "")
+                    }); 
+                } else {
+                    itemsOther.Add(new Item {
+                        fileName = Path.GetFileName(file),
+                        path = file.Replace(Path.GetFullPath(Path.Combine(rootPath.Text, @"..\")), "")
+                    }); 
+                }
+            }
+
+            // DataTable
+            DataTable f1Table = new DataTable();
+            DataTable f2Table = new DataTable();
+            DataTable othersTable = new DataTable();
+            f1Table.Columns.Add("File Name", typeof(string));
+            f1Table.Columns.Add("Path", typeof(string));
+
+            f2Table.Columns.Add("File Name", typeof(string));
+            f2Table.Columns.Add("Path", typeof(string));
+
+            othersTable.Columns.Add("File Name", typeof(string));
+            othersTable.Columns.Add("Path", typeof(string));
+
+            foreach (Item item in itemsF1)
+                f1Table.Rows.Add(item.fileName, item.path);
+            dataGridView1.DataSource = f1Table;
+
+            foreach (Item item in itemsF2)
+                f2Table.Rows.Add(item.fileName, item.path);
+            dataGridView2.DataSource = f2Table;
+
+            foreach (Item item in itemsOther)
+                othersTable.Rows.Add(item.fileName, item.path);
+            dataGridView3.DataSource = othersTable;
+
+
+            // Output
+            string f1Output = "", f2Output = "", othersOutput = "";
+            foreach (Item item in itemsF1)
+                f1Output += item.path + ", ";
+
+            if (f1Output.Length > 2)
+                f1RTB.Text = f1Output.Substring(0, f1Output.Length - 2);
+
+            foreach (Item item in itemsF2)
+                f2Output += item.path + ", ";
+
+            if (f2Output.Length > 2)
+                f2RTB.Text = f2Output.Substring(0, f2Output.Length - 2);
+
+            foreach (Item item in itemsOther)
+                othersOutput += item.path + ", ";
+
+            if (othersOutput.Length > 2)
+                othersRTB.Text = othersOutput.Substring(0, othersOutput.Length - 2);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e) {
+            rootPath.Width = (int)(Width * 0.8);
+            tabControl1.Height = (int)(Height * 0.3);
+            tableLayoutPanel2.Height = Height - panel1.Height - tabControl1.Height - tableLayoutPanel1.Height - 50;
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e) {
+            f1Label.Text = f1Box.Text;
+            f2Label.Text = F2Box.Text;
+        }
+
+        public class Item {
+            public string fileName { get; set; }
+            public string path { get; set; }
+        }
+    }
+}
